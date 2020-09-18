e5616a71d53d695882f41635277c2966614d49e3
Nurik <nurik080603@gmail.com>
2020-09-18 17:22:50 +0300

    Initial

---
+++ module 1/app.py
@@ -0,0 +1,20 @@
+slide1 = '(^_^)'
+slide2 = '(^_~)'
+slide3 = '(^_^)'
+slide4 = '(о_О)'
+slide5 = '(O_O)'
+slide6 = 'Привет, Мир!'
+
+print(slide1)
+print(slide2)
+print(slide3)
+print(slide4)
+print(slide5)
+print(slide6)
+
+
+
+
+
+
+
---
+++ module 2/app.py
@@ -0,0 +1,23 @@
+import time
+
+
+slide1 = '(^_^)'
+slide2 = '(^_~)'
+slide3 = '(^_^)'
+slide4 = '(о_О)'
+slide5 = '(O_O)'
+slide6 = 'Hello, World!'
+
+time.sleep(5)
+print(slide1)
+time.sleep(5)
+print(slide2)
+time.sleep(5)
+print(slide3)
+time.sleep(5)
+print(slide4)
+time.sleep(5)
+print(slide5)
+time.sleep(5)
+print(slide6)
+
